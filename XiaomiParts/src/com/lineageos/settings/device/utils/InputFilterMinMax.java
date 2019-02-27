MiracleDROID-HnT/android_device_xiaomi_mido

mido: XiaomiParts: add kcal support
taken from:
https://github.com/omnirom/android_device_leeco_zl1/commit/cdb1b9e2ae7f7612e0ca391c33e68ddedb181dce
credits: @darkobas

Also includes commits from:
https://github.com/Quantummech2000/DT-zenfone3

Signed-off-by: Nick <nickgunale@gmail.com>

XiaomiParts/src/com/mdroid/settings/device/utils/InputFilterMinMax.java
@@ -1,0 +1,36 @@
+package com.mdroid.settings.device.utils;
+
+import android.text.InputFilter;
+import android.text.Spanned;
+
+/**
+ * Created by aderendyaev at 14.02.18.
+ */
+
+public class InputFilterMinMax implements InputFilter{
+    private int min, max;
+
+    public InputFilterMinMax(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public InputFilterMinMax(String min, String max) {
+        this.min = Integer.parseInt(min);
+        this.max = Integer.parseInt(max);
+    }
+
+    @Override
+    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+        try {
+            int input = Integer.parseInt(dest.toString() + source.toString());
+            if (isInRange(min, max, input))
+                return null;
+        } catch (NumberFormatException nfe) { }
+        return "";
+    }
+
+    private boolean isInRange(int a, int b, int c) {
+        return b > a ? c >= a && c <= b : c >= b && c <= a;
+    }
+}

XiaomiParts/src/com/mdroid/settings/device/utils/NonScrollListView.java
@@ -1,0 +1,28 @@
+package com.mdroid.settings.device.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+public class NonScrollListView extends ListView {
+
+    public NonScrollListView(Context context) {
+        super(context);
+    }
+    public NonScrollListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public NonScrollListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int heightMeasureSpec_custom = View.MeasureSpec.makeMeasureSpec(
+                Integer.MAX_VALUE >> 2, View.MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec_custom);
+        ViewGroup.LayoutParams params = getLayoutParams();
+        params.height = getMeasuredHeight();
+    }
+}

XiaomiParts/src/com/mdroid/settings/device/utils/SeekBarPreference.java
@@ -1,0 +1,145 @@
+/*
+ *  Copyright (C) 2013 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.mdroid.settings.device.utils;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.preference.Preference;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import android.os.Handler;
+import android.os.Message;
+
+import com.mdroid.settings.device.R;
+
+public class SeekBarPreference extends Preference {
+
+    public int minimum = 1;
+    public int maximum = 256;
+    public int def = 256;
+    public int interval = 1;
+
+    final int UPDATE = 0;
+ 
+
+    int currentValue = def;
+
+    private OnPreferenceChangeListener changer;
+
+    public SeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SeekBarPreference, 0, 0);
+        
+        minimum = typedArray.getInt(R.styleable.SeekBarPreference_min_value, minimum);
+        maximum = typedArray.getInt(R.styleable.SeekBarPreference_max_value, maximum);
+        def = typedArray.getInt(R.styleable.SeekBarPreference_default_value, def);
+
+        typedArray.recycle();
+	}
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+    }
+
+    private void bind(final View layout) {
+        final EditText monitorBox = (EditText) layout.findViewById(R.id.monitor_box);
+        final SeekBar bar = (SeekBar) layout.findViewById(R.id.seek_bar);
+
+        monitorBox.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        bar.setMax(maximum - minimum);
+        bar.setProgress(currentValue - minimum);
+
+        monitorBox.setText(String.valueOf(currentValue));
+        monitorBox.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus)
+                    monitorBox.setSelection(monitorBox.getText().length());
+            }
+        });
+        monitorBox.setFilters(new InputFilter[]{new InputFilterMinMax(minimum, maximum)});
+        monitorBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int keyCode, KeyEvent event) {
+                if (keyCode == EditorInfo.IME_ACTION_DONE) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    currentValue = Integer.parseInt(v.getText().toString());
+                    bar.setProgress(currentValue - minimum, true);
+                    changer.onPreferenceChange(SeekBarPreference.this, Integer.toString(currentValue));
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        bar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progress = Math.round(((float) progress) / interval) * interval;
+                currentValue = progress + minimum;
+                monitorBox.setText(String.valueOf(currentValue));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                changer.onPreferenceChange(SeekBarPreference.this, Integer.toString(currentValue));
+            }
+        });
+    }
+
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+        bind(view);
+    }
+
+    public void setInitValue(int progress) {
+        currentValue = progress;
+    }
+
+    @Override
+    public void setOnPreferenceChangeListener(OnPreferenceChangeListener onPreferenceChangeListener) {
+        changer = onPreferenceChangeListener;
+        super.setOnPreferenceChangeListener(onPreferenceChangeListener);
+    }
+
+    public int reset() {
+        currentValue = def;
+        notifyChanged();
+        return def;
+    }
+}

Roker2/android_device_xiaomi_mido-1

XiaomiParts/src/com/lineageos/settings/device/utils/InputFilterMinMax.java
@@ -1,0 +1,36 @@
+package com.lineageos.settings.device.utils;
+
+import android.text.InputFilter;
+import android.text.Spanned;
+
+/**
+ * Created by aderendyaev at 14.02.18.
+ */
+
+public class InputFilterMinMax implements InputFilter{
+    private int min, max;
+
+    public InputFilterMinMax(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public InputFilterMinMax(String min, String max) {
+        this.min = Integer.parseInt(min);
+        this.max = Integer.parseInt(max);
+    }
+
+    @Override
+    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+        try {
+            int input = Integer.parseInt(dest.toString() + source.toString());
+            if (isInRange(min, max, input))
+                return null;
+        } catch (NumberFormatException nfe) { }
+        return "";
+    }
+
+    private boolean isInRange(int a, int b, int c) {
+        return b > a ? c >= a && c <= b : c >= b && c <= a;
+    }
+}

XiaomiParts/src/com/lineageos/settings/device/utils/NonScrollListView.java
@@ -1,0 +1,28 @@
+package com.lineageos.settings.device.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+public class NonScrollListView extends ListView {
+
+    public NonScrollListView(Context context) {
+        super(context);
+    }
+    public NonScrollListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public NonScrollListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int heightMeasureSpec_custom = View.MeasureSpec.makeMeasureSpec(
+                Integer.MAX_VALUE >> 2, View.MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec_custom);
+        ViewGroup.LayoutParams params = getLayoutParams();
+        params.height = getMeasuredHeight();
+    }
+}

XiaomiParts/src/com/lineageos/settings/device/utils/SeekBarPreference.java
@@ -1,0 +1,145 @@
+/*
+ *  Copyright (C) 2013 The OmniROM Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package com.lineageos.settings.device.utils;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.preference.Preference;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import android.os.Handler;
+import android.os.Message;
+
+import com.lineageos.settings.device.R;
+
+public class SeekBarPreference extends Preference {
+
+    public int minimum = 1;
+    public int maximum = 256;
+    public int def = 256;
+    public int interval = 1;
+
+    final int UPDATE = 0;
+ 
+
+    int currentValue = def;
+
+    private OnPreferenceChangeListener changer;
+
+    public SeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs, 0);
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SeekBarPreference, 0, 0);
+        
+        minimum = typedArray.getInt(R.styleable.SeekBarPreference_min_value, minimum);
+        maximum = typedArray.getInt(R.styleable.SeekBarPreference_max_value, maximum);
+        def = typedArray.getInt(R.styleable.SeekBarPreference_default_value, def);
+
+        typedArray.recycle();
+	}
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+    }
+
+    private void bind(final View layout) {
+        final EditText monitorBox = (EditText) layout.findViewById(R.id.monitor_box);
+        final SeekBar bar = (SeekBar) layout.findViewById(R.id.seek_bar);
+
+        monitorBox.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        bar.setMax(maximum - minimum);
+        bar.setProgress(currentValue - minimum);
+
+        monitorBox.setText(String.valueOf(currentValue));
+        monitorBox.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus)
+                    monitorBox.setSelection(monitorBox.getText().length());
+            }
+        });
+        monitorBox.setFilters(new InputFilter[]{new InputFilterMinMax(minimum, maximum)});
+        monitorBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int keyCode, KeyEvent event) {
+                if (keyCode == EditorInfo.IME_ACTION_DONE) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    currentValue = Integer.parseInt(v.getText().toString());
+                    bar.setProgress(currentValue - minimum, true);
+                    changer.onPreferenceChange(SeekBarPreference.this, Integer.toString(currentValue));
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        bar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progress = Math.round(((float) progress) / interval) * interval;
+                currentValue = progress + minimum;
+                monitorBox.setText(String.valueOf(currentValue));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                changer.onPreferenceChange(SeekBarPreference.this, Integer.toString(currentValue));
+            }
+        });
+    }
+
+    @Override
+    protected void onBindView(View view) {
+        super.onBindView(view);
+        bind(view);
+    }
+
+    public void setInitValue(int progress) {
+        currentValue = progress;
+    }
+
+    @Override
+    public void setOnPreferenceChangeListener(OnPreferenceChangeListener onPreferenceChangeListener) {
+        changer = onPreferenceChangeListener;
+        super.setOnPreferenceChangeListener(onPreferenceChangeListener);
+    }
+
+    public int reset() {
+        currentValue = def;
+        notifyChanged();
+        return def;
+    }
+}