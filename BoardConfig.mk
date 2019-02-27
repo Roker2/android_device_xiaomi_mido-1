6ff63aedca44d7889bbd7b4319858185ba8ed610
Roker2 <dimaminko2000@gmail.com>
2019-02-27 07:02:03 +0000

    Revert "mido: Adapt for local LineageHW java overlays"
    This reverts commit ad9d0cfacd6202bd9630012a2f172c887ed4d8e4.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,6 +168,10 @@ DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 TARGET_INIT_VENDOR_LIB := libinit_msm8953
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 
+# Lineage Hardware
+BOARD_HARDWARE_CLASS += \
+    $(VENDOR_PATH)/lineagehw
+
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
