06357f4aa3bfaa073a6e12059a0b46afb6e86b75
Roker2 <dimaminko2000@gmail.com>
2019-04-05 19:56:47 +0000

    mido: XiaomiParts: Delete usb fastcharge toggle and spectrum
    Revert "mido: XiaomiParts: add usb fastcharge toggle"
    
    This reverts commit 4eda9a11de7ce9305f0cb88204bb9c2f085026d0.
    
    Revert "XiaomiParts: Spectrum profiles: Change frequencies"
    
    This reverts commit 3e1b19a6172087746fd2c6e785593d3335b7d1a1.
    
    Revert "mido: XiaomiParts: add spectrum support"
    
    This reverts commit ef91c8295de0c07fbe1c653495195c33aeba27e7.
    
    Change-Id: I7a08e985e7db0fa8fed55580f3c7fcc941e58809

--- XiaomiParts/src/com/lineageos/settings/device/DeviceSettings.java
+++ XiaomiParts/src/com/lineageos/settings/device/DeviceSettings.java
@@ -19,16 +19,12 @@ package com.lineageos.settings.device;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.SystemProperties;
 import android.support.v14.preference.PreferenceFragment;
-import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceClickListener;
@@ -52,29 +48,17 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final String KEY_YELLOW_TORCH_BRIGHTNESS = "yellow_torch_brightness";
     public static final String KEY_WHITE_TORCH_BRIGHTNESS = "white_torch_brightness";
     public static final String KEY_GLOVE_MODE = "glove_mode";
-    public static final String USB_FASTCHARGE_KEY = "fastcharge";
-    public static final String USB_FASTCHARGE_PATH = "/sys/kernel/fast_charge/force_fast_charge";
-
-    private static final String SPECTRUM_KEY = "spectrum";
-    private static final String SPECTRUM_SYSTEM_PROPERTY = "persist.spectrum.profile";
-
-    private static final String KEY_CATEGORY_USB_FASTCHARGE = "usb_fastcharge";
 
     private VibratorStrengthPreference mVibratorStrength;
     private YellowTorchBrightnessPreference mYellowTorchBrightness;
     private WhiteTorchBrightnessPreference mWhiteTorchBrightness;
     private TwoStatePreference mGloveMode;
-    private ListPreference mSpectrum;
-    private SwitchPreference mFastcharge;
-    private PreferenceCategory mUsbFastcharge;
 
     private static final String GLOVE_MODE_FILE = "/sys/devices/virtual/tp_glove/device/glove_enable";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.main, rootKey);
-
-        PreferenceScreen prefSet = getPreferenceScreen();
 
         PreferenceScreen mKcalPref = (PreferenceScreen) findPreference("kcal");
         mKcalPref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
@@ -104,26 +88,6 @@ public class DeviceSettings extends PreferenceFragment implements
         mGloveMode = (TwoStatePreference) findPreference(KEY_GLOVE_MODE);
         mGloveMode.setChecked(PreferenceManager.getDefaultSharedPreferences(getContext()).getBoolean(DeviceSettings.KEY_GLOVE_MODE, false));
         mGloveMode.setOnPreferenceChangeListener(this);
-
-        mSpectrum = (ListPreference) findPreference(SPECTRUM_KEY);
-        if( mSpectrum != null ) {
-            mSpectrum.setValue(SystemProperties.get(SPECTRUM_SYSTEM_PROPERTY, "0"));
-            mSpectrum.setOnPreferenceChangeListener(this);
-            mSpectrum.setSummary(mSpectrum.getEntry());
-        }
-
-        if (Utils.fileWritable(USB_FASTCHARGE_PATH)) {
-          mFastcharge = (SwitchPreference) findPreference(USB_FASTCHARGE_KEY);
-          mFastcharge.setChecked(Utils.getFileValueAsBoolean(USB_FASTCHARGE_PATH, false));
-          mFastcharge.setOnPreferenceChangeListener(this);
-        } else {
-          mUsbFastcharge = (PreferenceCategory) prefSet.findPreference("usb_fastcharge");
-          prefSet.removePreference(mUsbFastcharge);
-        }
-    }
-
-    private void setFastcharge(boolean value) {
-            Utils.writeValue(USB_FASTCHARGE_PATH, value ? "1" : "0");
     }
 
     public static void restore(Context context) {
@@ -143,27 +107,7 @@ public class DeviceSettings extends PreferenceFragment implements
             SharedPreferences.Editor prefChange = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
             prefChange.putBoolean(KEY_GLOVE_MODE, enabled).commit();
             Utils.writeValue(GLOVE_MODE_FILE, enabled ? "1" : "0");
-            return true;
-        } else if (preference == mSpectrum) {
-            String strvalue = (String) newValue;
-            int index = mSpectrum.findIndexOfValue(strvalue);
-            mSpectrum.setSummary(mSpectrum.getEntries()[index]);
-            SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, strvalue);
-            return true;
-        } else if (preference == mFastcharge) {
-            boolean value = (Boolean) newValue;
-            mFastcharge.setChecked(value);
-            setFastcharge(value);
-            SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
-            editor.putBoolean(USB_FASTCHARGE_KEY, value);
-            editor.commit();
-            return true;
         }
-        return false;
-    }
-
-    public static void restoreSpectrumProp(Context context) {
-        String spectrumStoredValue = PreferenceManager.getDefaultSharedPreferences(context).getString(SPECTRUM_KEY, "0");
-        SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, spectrumStoredValue);
+        return true;
     }
 }
--- XiaomiParts/src/com/lineageos/settings/device/Startup.java
+++ XiaomiParts/src/com/lineageos/settings/device/Startup.java
@@ -51,9 +51,6 @@ public class Startup extends BroadcastReceiver {
         WhiteTorchBrightnessPreference.restore(context);
         YellowTorchBrightnessPreference.restore(context);
         DeviceSettings.restore(context);
-        DeviceSettings.restoreSpectrumProp(context);
-        boolean usbFastchargeStoredValue = PreferenceManager.getDefaultSharedPreferences(context).getBoolean(DeviceSettings.USB_FASTCHARGE_KEY, false);
-        Utils.writeValue(DeviceSettings.USB_FASTCHARGE_PATH, usbFastchargeStoredValue ? "1" : "0" );
         DisplayCalibration.restore(context);
     }
 }
