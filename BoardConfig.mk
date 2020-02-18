zeelog/android_device_xiaomi_mido

mido: Correct define libinit variable
Fixes:

15:55:35 error: system/core/init/Android.bp:95:1: "libinit" depends on undefined module "//device/xiaomi/mido/init/libinit_mido"

Change-Id: Ia1a58bea3df625fd76c060370dc7f9fa2a597765

BoardConfig.mk
@@ -151,7 +151,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_mido
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_mido
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 
 # Lights

Roker2/android_device_xiaomi_mido-1

BoardConfig.mk
@@ -166,7 +166,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_msm8953
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_msm8953
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 
 # Lights